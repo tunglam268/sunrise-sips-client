84fa245ea2b9ad328cc08562527f9d9c8ce9af9a
tunglam268 <ngtunglam26899@gmail.com>
2024-08-02 14:12:45 +0700

    Add change

--- app/layout.js
+++ app/layout.js
@@ -18,6 +18,7 @@ export default function RootLayout({children}) {
     return (
         <html lang="en">
         <body className={`${lora.variable} ${poppins.variable}`}>{children}</body>
+        <SpeedInsights />
         </html>
     );
 }
