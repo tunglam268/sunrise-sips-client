d714d8dfe28a2342fea10a4db00c687feb90bffc
namtc07 <truongcongnam1207@gmail.com>
2024-09-12 18:19:27 +0700

    coding menu

--- app/menu/page.js
+++ app/menu/page.js
@@ -1,9 +1,8 @@
-import dynamic from "next/dynamic";
+"use client";
 import Image from "next/image";
-import { FaFacebookF } from "react-icons/fa6";
-
-const Header = dynamic(() => import("../../components/Header"), {});
-const Footer = dynamic(() => import("../../components/Footer"), { ssr: false });
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { FaArrowLeft } from "react-icons/fa6";
 
 const mockGlutenFree = [
   {
@@ -117,38 +116,186 @@ const mockVegeterian = [
   },
 ];
 
+const mockSmoothie = [
+  {
+    isAttention: true,
+    name: "MATCHA MANGO SMOOTHIE",
+    price: 10,
+    description: "Mango, matcha, vanilla yogurt, mango juice, lemon sorbet.",
+  },
+  {
+    isAttention: false,
+    name: "MIXED BERRIES SMOOTHIE",
+    price: 10,
+    description: "Mixed berries, vanilla yogurt, honey, milk, ice.",
+  },
+  {
+    isAttention: true,
+    name: "OREO CHOCO SMOOTHIE",
+    price: 10,
+    description: "Oreo, 33% cacao chocolate, vanilla ice cream, banana, milk, ice.",
+  },
+  {
+    isAttention: false,
+    name: "BANANA SMOOTHIE",
+    price: 10,
+    description: "Banana, vanilla yogurt, honey, ice.",
+  },
+];
+
+const mockMilkshake = [
+  {
+    isAttention: false,
+    name: "CHOCOLATE",
+    price: 8,
+  },
+  {
+    isAttention: false,
+    name: "VANILLA",
+    price: 8,
+  },
+  {
+    isAttention: false,
+    name: "STRAWBERRY",
+    price: 8,
+  },
+];
+
+const mockJuice = [
+  {
+    isAttention: false,
+    name: "ORANGE JUICE",
+    price: 8,
+  },
+  {
+    isAttention: false,
+    name: "BOOST JUICE",
+    description: "Carrot, orange, ginger.",
+    price: 8,
+  },
+  {
+    isAttention: false,
+    name: "POWER PUNCH",
+    description: "Beetroot, carrot, apple, ginger.",
+    price: 9,
+  },
+  {
+    isAttention: false,
+    name: "GO GREEN",
+    description: "Cucumber, celery, lemon, apple.",
+    price: 9,
+  },
+];
+
+const mockTeapot = [
+  {
+    name: "CHAMOMILE TEA",
+  },
+  {
+    name: "ENGLISH BREAKFAST TEA",
+  },
+  {
+    name: "FRENCH EARL GREY TEA",
+  },
+  {
+    name: "LEMONGRASS & GINGER TEA",
+  },
+  {
+    name: "PEPPERMINT TEA",
+  },
+  {
+    name: "SENCHA GREEN TEA",
+  },
+];
+
+const mockMatchaDrink = [
+  {
+    name: "STRAWBERRY MATCHA",
+    description: "Naked matcha, strawberry, milk, ice.",
+    price: 10,
+  },
+  {
+    name: "ICED COCONUT MATCHA",
+    description: "Naked matcha, coconut, milk, ice.",
+    price: 10,
+  },
+];
+
+const mockCoffee = [
+  {
+    name: "CAPUCHINO",
+    reg: 4.5,
+    lrg: 5,
+  },
+  {
+    name: "LATTE",
+    reg: 4.5,
+    lrg: 5,
+  },
+  {
+    name: "FLAT WHITE ",
+    reg: 4.5,
+    lrg: 5,
+  },
+  {
+    name: "MOCCA",
+    reg: 5,
+    lrg: 5.5,
+  },
+  {
+    name: "MATCHA LATTE",
+    reg: 5,
+    lrg: 5.5,
+  },
+  {
+    name: "HOT CHOCOLATE",
+    reg: 5,
+    lrg: 5.5,
+  },
+  {
+    name: "STICKY CHAI",
+    reg: null,
+    lrg: 5.5,
+  },
+  {
+    name: "BABYCHINO ",
+    reg: 2,
+    lrg: null,
+  },
+];
+
 const imagesVegan = [
   {
-    src: "../../public/menu/chicken-karaage-burger.png",
+    src: "/menu/chicken-karaage-burger.png",
     alt: "chicken-karaage-burger",
   },
   {
-    src: "../../public/menu/chilli-prawn-croissant.png",
+    src: "/menu/chilli-prawn-croissant.png",
     alt: "chilli-prawn-croissant",
   },
   {
-    src: "../../public/menu/soba-noodles.png",
+    src: "/menu/soba-noodles.png",
     alt: "soba-noodles",
   },
 ];
 
-const MenuItem = ({ source = [] }) => {
+const MenuItem = ({ source = [], isOption = true }) => {
   return source.map((item) => (
-    <div className={`flex gap-2 justify-${"end"} w-[500px] max-w-[100%] p-3`}>
+    <div className={`flex gap-2 justify-${"end"} max-w-[400px] w-[100%]`}>
       <div className={`${item?.isAttention ? "z-0" : "-z-10"}`}>*</div>
       <div className="flex flex-col gap-1 w-[100%]">
         <div className="flex justify-between">
-          <div className="font-medium">{item?.name}</div>
-          <div className="font-medium">{item?.price}</div>
+          <div className="font-medium text-sm md:text-base">{item?.name}</div>
+          <div className="font-medium text-sm md:text-base">{item?.price}</div>
         </div>
-        {item?.description && <div className="font-extralight">{item?.description}</div>}
+        {item?.description && <div className="font-extralight text-xs md:text-sm">{item?.description}</div>}
         {item?.options?.length && (
           <div>
             <div className="flex gap-3">
-              <div>Options:</div>
+              <div className="text-xs md:text-sm">{isOption ? "Options" : "Extras (+2)"}:</div>
               <div className="flex flex-wrap gap-x-3 font-light">
                 {item?.options?.map?.((option) => (
-                  <div>{option}</div>
+                  <div className="text-xs md:text-sm">{option}</div>
                 ))}
               </div>
             </div>
@@ -160,62 +307,258 @@ const MenuItem = ({ source = [] }) => {
 };
 
 const ImageList = ({ data }) => {
-  return data?.map?.((image) => {
-    return <Image src={image?.src} width={500} height={500} alt={image?.alt} />;
+  return data?.map?.((image, index) => {
+    return (
+      <div key={index}>
+        <Image src={image.src} width={300} height={300} alt={image.alt} className="object-cover w-full h-full" />
+      </div>
+    );
   });
 };
 
-function Menu() {
+const ImageGrid = () => {
   return (
-    <main className={"bg-menu2 w-full bg-cover bg-white mx-auto overflow-hidden relative"}>
-      <div className="p-4">
-        <div className="flex flex-wrap  p-1 gap-10  justify-center">
-          <div className="flex flex-col gap-4">
-            <div>(GF): Gluten Free</div>
-            <div className="flex flex-col gap-4">
-              <MenuItem source={mockGlutenFree} />
-            </div>
-          </div>
-          <div className="flex flex-col gap-4">
-            <div>(VE): Vegeterian</div>
-            <div className="flex flex-col gap-4">
-              <MenuItem source={mockVegeterian} />
-            </div>
-          </div>
-          <div className="flex flex-col gap-4">
-            <div>(V): Vegan</div>
-            {/* <ImageList data={imagesVegan} /> */}
-            <Image src={"../../public/menu/chicken-karaage-burger.png"} width={500} height={500} />
-            <div className="flex flex-col gap-2">
-              <div className="px-4 font-bold">KIDS MENU</div>
-              <MenuItem
-                source={[
-                  {
-                    isAttention: false,
-                    name: "KIDS FISH AND CHIPS",
-                    price: 13.9,
-                    description: "Golden crispy battered fish and tasty chips.",
-                  },
-                  {
-                    isAttention: false,
-                    name: "KIDS CHEESE BURGER",
-                    price: 13.9,
-                    description: "Milk bun, beef patty, tomato sauce.",
-                  },
-                ]}
-              />
-            </div>
-            <div className="flex flex-col gap-2">
-              <div className="px-4 font-bold">TO SHARE</div>
-              <MenuItem
-                source={[
-                  { isAttention: false, name: "POTATO CHIPS (V) (GF)", price: 8.5 },
-                  { isAttention: false, name: "SWEET POTATO CHIPS (V) (GF)", price: 9.5 },
-                ]}
-              />
-            </div>
-          </div>
+    <div className="flex">
+      {/* Cột 1: Hình 1 */}
+      <div className="w-1/2">
+        <Image
+          src={imagesVegan[0].src}
+          width={150}
+          height={100}
+          alt={imagesVegan[0].alt}
+          className="object-cover w-full h-full"
+        />
+      </div>
+
+      {/* Cột 2: 2 Hình chồng nhau */}
+      <div className="w-1/2 flex flex-col">
+        <div className="flex-1">
+          <Image
+            src={imagesVegan[1].src}
+            width={150}
+            height={50}
+            alt={imagesVegan[1].alt}
+            className="object-cover w-full h-full"
+          />
         </div>
+        <div className="flex-1">
+          <Image
+            src={imagesVegan[2].src}
+            width={150}
+            height={50}
+            alt={imagesVegan[2].alt}
+            className="object-cover w-full h-full"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const CATEGORIES = {
+  FOODS: "FOODS",
+  DRINKS: "DRINKS",
+};
+
+const menuList = [
+  {
+    href: "FOODS",
+    title: "Foods",
+    className: "inline-block p-3",
+  },
+  {
+    href: "DRINKS",
+    title: "Drinks",
+    className: "inline-block p-3",
+  },
+];
+
+function Menu() {
+  const [hrefSelected, setHrefSelected] = useState(CATEGORIES.FOODS);
+
+  useEffect(() => {
+    const hash = window.location.hash.substring(1);
+    setHrefSelected(hash);
+  }, []);
+
+  return (
+    <main className={"bg-menu2 w-full bg-cover bg-white mx-auto"}>
+      <div class="text-sm font-medium text-center bg-brown-heavy fixed top-0 w-full z-50 left-0 right-0 transition-all duration-200">
+        <div className="max-w-[1440px] m-auto relative">
+          <div className="absolute left-6 top-[35%]">
+            <Link href="/">
+              <FaArrowLeft color="white" />
+            </Link>
+          </div>
+          <ul class="flex gap-3 flex-wrap m-auto justify-center pb-1">
+            {menuList?.map((menu, index) => (
+              <li key={index} onClick={() => setHrefSelected(menu.href)}>
+                <a href={`#${menu.href}`} className={`${menu.className} ${hrefSelected === menu.href && "border-b-2"}`}>
+                  {menu.title}
+                </a>
+                 
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className="gap-10 w-[100%] max-w-[1440px] m-auto py-8 mt-10">
+        {hrefSelected === CATEGORIES.FOODS && (
+          <div className="md:px-5 px-4 pt-1">
+            <div className="flex flex-wrap md:justify-between justify-center gap-4 px-2 md:px-0">
+              <div className="flex flex-col gap-4">
+                <div className="flex flex-col gap-4">
+                  <MenuItem source={mockGlutenFree} />
+                </div>
+              </div>
+              <div className="flex flex-col gap-4">
+                <div className="flex flex-col gap-4">
+                  <MenuItem source={mockVegeterian} />
+                </div>
+              </div>
+              <div className="flex flex-col gap-4">
+                <div className="flex flex-col gap-2">
+                  <div className="px-4 font-bold underline underline-offset-4">KIDS MENU</div>
+                  <MenuItem
+                    source={[
+                      {
+                        isAttention: false,
+                        name: "KIDS FISH AND CHIPS",
+                        price: 13.9,
+                        description: "Golden crispy battered fish and tasty chips.",
+                      },
+                      {
+                        isAttention: false,
+                        name: "KIDS CHEESE BURGER",
+                        price: 13.9,
+                        description: "Milk bun, beef patty, tomato sauce.",
+                      },
+                    ]}
+                  />
+                </div>
+                <div className="flex flex-col gap-2">
+                  <div className="px-4 font-bold underline underline-offset-4">TO SHARE</div>
+                  <MenuItem
+                    source={[
+                      { isAttention: false, name: "POTATO CHIPS (V) (GF)", price: 8.5 },
+                      { isAttention: false, name: "SWEET POTATO CHIPS (V) (GF)", price: 9.5 },
+                    ]}
+                  />
+                </div>
+                <div className="max-w-[500px] text-wrap px-4 italic text-sm mt-10">
+                  Please let us know prior to ordering if you have any allergies, we will try our best to accommodate.
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="text-center m-auto pt-8 font-semibold text-xl">PLEASE ORDER AT THE COUNTER</div>
+            </div>
+          </div>
+        )}
+        {hrefSelected === CATEGORIES.DRINKS && (
+          <div className="flex gap-10 h-[100%] my-28">
+            <div className="w-1/2">
+              <div className="font-bold text-2xl pb-3">SMOOTHIE, ACAI & MORE</div>
+              <div className="gap-8">
+                <div className="border p-5 flex gap-16">
+                  <div class="grid grid-cols-2 gap-4 divide-x">
+                    <div className="left flex flex-col gap-4">
+                      <div className="smoothie">
+                        <div className="underline underline-offset-8 decoration-dashed font-semibold ml-4 pb-4">
+                          SMOOTHIE
+                        </div>
+                        <div className="flex flex-col gap-4">
+                          <MenuItem source={mockSmoothie} />
+                        </div>
+                      </div>
+                      <div className="milkshake">
+                        <div className="underline underline-offset-8 decoration-dashed font-semibold ml-4 pb-4">
+                          MILKSHAKE
+                        </div>
+                        <div className="flex flex-col gap-4">
+                          <MenuItem source={mockMilkshake} />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="right flex flex-col gap-4">
+                      <div className="acaibowl">
+                        <div className="underline underline-offset-8 decoration-dashed font-semibold ml-4 pb-4">
+                          ACAI BOWL ($15)
+                        </div>
+                        <div className="flex flex-col gap-4">
+                          <MenuItem
+                            source={[
+                              {
+                                description:
+                                  "Acai smoothly blended with ripe bananas and a splash of coconut juice, topped with crunchy granola, coconut flakes, a drizzle of honey and fresh seasonal fruit.",
+                                options: ["Nutella", "Granola", "Peanut butter", "Fruit"],
+                              },
+                            ]}
+                          />
+                        </div>
+                      </div>
+                      <div className="juice">
+                        <div className="underline underline-offset-8 decoration-dashed font-semibold ml-4 pb-4">
+                          ACAI BOWL ($15)
+                        </div>
+                        <div className="flex flex-col gap-4">
+                          <MenuItem source={mockJuice} />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="w-1/2">
+              <div className="font-bold text-2xl text-right pb-3">COFFEE, TEA & MATCHA</div>
+              <div className="gap-8">
+                <div className="border p-5 flex gap-16 justify-center">
+                  <div class="grid grid-cols-2 gap-4 divide-x">
+                    <div className="left flex flex-col gap-4">
+                      <div className="coffee">
+                        <div className="grid grid-cols-6 gap-4">
+                          <div className="col-span-4 underline underline-offset-8 decoration-dashed font-semibold pb-4">
+                            COFFEE
+                          </div>
+                          <div className="col-start-5 row-start-1 font-semibold">REG</div>
+                          <div className="col-start-6 row-start-1 font-semibold">LRG</div>
+                        </div>
+                        <div className="flex flex-col gap-4">
+                          {mockCoffee?.map((c) => (
+                            <div className="grid grid-cols-6 gap-4">
+                              <div className="col-span-4 font-medium">{c.name}</div>
+                              <div className="col-start-5 row-start-1 text-center font-medium">{c.reg}</div>
+                              <div className="col-start-6 row-start-1 text-center font-medium">{c.lrg}</div>
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                    </div>
+                    <div className="right flex flex-col gap-4">
+                      <div className="teapot">
+                        <div className="underline underline-offset-8 decoration-dashed font-semibold ml-4 pb-4">
+                          TEA POT ($5)
+                        </div>
+                        <div className="flex flex-col gap-4">
+                          <MenuItem source={mockTeapot} />
+                        </div>
+                      </div>
+                      <div className="teapot">
+                        <div className="underline underline-offset-8 decoration-dashed font-semibold ml-4 pb-4">
+                          MATCHA DRINKS
+                        </div>
+                        <div className="flex flex-col gap-4">
+                          <MenuItem source={mockMatchaDrink} />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </main>
   );
--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    loader: "akamai",
-    path: "",
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
